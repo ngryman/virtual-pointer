9a74c4148cfb1ca4b4c013fc269784dbaef1f1d1
Nicolas Gryman <ngryman@gmail.com>
2013-03-16 20:26:35 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# jQuery Finger <sup>0.0.8</sup>
+
+jQuery tap & gestures, fingers in the nose.
+
+**jQuery Finger** unifies click and touch events by removing the 300ms delay on touch devices. It also provide a common
+set of events to handle basic gestures such as drag and pinch.
+Small (< 1kb gzipped), it is focused on performance, is well tested and ... also supports jQuery delegated events.
+
+[![Build Status](https://travis-ci.org/ngryman/jquery.finger.png)](https://travis-ci.org/ngryman/jquery.finger)
+[![Build Status](https://gemnasium.com/ngryman/jquery.finger.png)](https://gemnasium.com/ngryman/jquery.finger)
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/ngryman/jquery.finger/master/dist/jquery.finger.min.js
+[max]: https://raw.github.com/ngryman/jquery.finger/master/dist/jquery.finger.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/jquery.finger.min.js"></script>
+<script>
+  // direct event
+  $('.touchme').on('tap', function() {
+    console.log('direct');
+  });
+
+  // delegated event
+  $('body').on('tap', '.touchme', function() {
+    console.log('delegated');
+  });
+});
+</script>
+```
+
+## Documentation
+
+### Gestures
+
+          | tap | doubletap | press | drag | flick | pinch | spread | rotate
+----------|-----|-----------|-------|------|-------|-------|--------|-------
+Available |  ✔ |     ✔     |   ✔  |  ✔  |   ✔  |       |        |
+
+Here is a complete list of gestures that will *probably* be supported: http://smustalks.appspot.com/io-12/#44
+
+## Examples
+_(Coming soon)_
+
+## Notes
+
+When running tests in the browser, your mouse / finger must not interact with the page.
+
+## Release History
+
+### v0.0.8
+  - fixed bugs on delegated events.
+  - better cross-browser support (still needs some work/tests).
+  - internal refactoring for consistency and performance.
+
+### v0.0.7
+  - various cross browsers fixes.
+
+### v0.0.6
+  - updated description.
+
+### v0.0.5
+  - updated jquery manifest and published on http://plugins.jquery.com.
+
+### v0.0.4
+  - added `drag` and `flick` gestures.
+  - enhanced `event` object.
+  - internal refactoring for consistency.
+
+### v0.0.3
+  - migration to **grunt** 0.4.
+  - migration to **mocha** / **chaijs** for tests.
+
+### v0.0.2
+  - added `doubletap` and `press` gestures.
+  - internal refactoring for consistency and performance.
+
+### v0.0.1
+  - `tap` gesture first implementation.
---
+++ test/virtual-pointer_test.js
@@ -0,0 +1,186 @@
+/*global Mocha, describe, xdescribe, beforeEach, afterEach, it, xit, sinon*/
+
+(function($) {
+
+	/** test suite */
+
+	describe('Virtual Pointer', function() {
+		before(function() {
+			this.pointer = VirtualPointer(this);
+
+			// adjusting time values for testing purposes
+			this.pointer.DOUBLETAP_DURATION = 25;
+			this.pointer.PRESS_DURATION = 25;
+			this.pointer.FLICK_DURATION = 25;
+		});
+
+		afterEach(function() {
+			$('html, body').off();
+		});
+
+		describe('tap start', function() {
+			it('should work with direct events', function() {
+				var handler = sinon.spy();
+				$('body').on(this.pointer.START_EVENT, handler);
+				this.pointer.tapStart();
+				handler.should.have.been.calledOnce;
+				handler.should.have.been.calledOn(document.body);
+			});
+
+			it('should work with delegated events', function() {
+				var handler = sinon.spy();
+				$('html').on(this.pointer.START_EVENT, 'body', handler);
+				this.pointer.tapStart();
+				handler.should.have.been.calledOnce;
+				handler.should.have.been.calledOn(document.body);
+			});
+		});
+
+		describe('tap end', function() {
+			it('should work with direct events', function() {
+				var handler = sinon.spy();
+				$('body').on(this.pointer.STOP_EVENT, handler);
+				this.pointer.tapEnd();
+				handler.should.have.been.calledOnce;
+				handler.should.have.been.calledOn(document.body);
+			});
+
+			it('should work with delegated events', function() {
+				var handler = sinon.spy();
+				$('html').on(this.pointer.STOP_EVENT, 'body', handler);
+				this.pointer.tapEnd();
+				handler.should.have.been.calledOnce;
+				handler.should.have.been.calledOn(document.body);
+			});
+		});
+
+		describe('move', function() {
+			it('should work with direct events', function(done) {
+				var handler = sinon.spy();
+				$('body').on(this.pointer.MOVE_EVENT, handler);
+				this.pointer.move(100, 100, 25, function() {
+					handler.callCount.should.be.above(1);
+					handler.should.have.been.calledOn(document.body);
+					done();
+				});
+			});
+
+			it('should work with delegated events', function(done) {
+				var handler = sinon.spy();
+				$('html').on(this.pointer.MOVE_EVENT, 'body', handler);
+				this.pointer.move(100, 100, 25, function() {
+					handler.callCount.should.be.above(1);
+					handler.should.have.been.calledOn(document.body);
+					done();
+				});
+			});
+		});
+
+		describe('tap', function() {
+			it('should work with direct events', function() {
+				var handler = sinon.spy();
+				$('body').on(this.pointer.START_EVENT + ' ' + this.pointer.STOP_EVENT, handler);
+				this.pointer.tap();
+				handler.should.have.been.calledTwice;
+				handler.should.have.been.calledOn(document.body);
+			});
+
+			it('should work with delegated events', function() {
+				var handler = sinon.spy();
+				$('html').on(this.pointer.START_EVENT + ' ' + this.pointer.STOP_EVENT, 'body', handler);
+				this.pointer.tap();
+				handler.should.have.been.calledTwice;
+				handler.should.have.been.calledOn(document.body);
+			});
+		});
+
+		describe('press', function() {
+			it('should work with direct events', function(done) {
+				var handler = sinon.spy();
+				$('body').on(this.pointer.START_EVENT + ' ' + this.pointer.STOP_EVENT, handler);
+				this.pointer.press(function() {
+					handler.should.have.been.calledTwice;
+					handler.should.have.been.calledOn(document.body);
+					done();
+				});
+			});
+
+			it('should work with delegated events', function(done) {
+				var handler = sinon.spy();
+				$('html').on(this.pointer.START_EVENT + ' ' + this.pointer.STOP_EVENT, 'body', handler);
+				this.pointer.press(function() {
+					handler.should.have.been.calledTwice;
+					handler.should.have.been.calledOn(document.body);
+					done();
+				});
+			});
+		});
+
+		describe('double tap', function() {
+			it('should work with direct events', function(done) {
+				var handler = sinon.spy();
+				$('body').on(this.pointer.START_EVENT + ' ' + this.pointer.STOP_EVENT, handler);
+				this.pointer.doubleTap(function() {
+					handler.callCount.should.equal(4);
+					handler.should.have.been.calledOn(document.body);
+					done();
+				});
+			});
+
+			it('should work with delegated events', function(done) {
+				var handler = sinon.spy();
+				$('html').on(this.pointer.START_EVENT + ' ' + this.pointer.STOP_EVENT, 'body', handler);
+				this.pointer.doubleTap(function() {
+					handler.callCount.should.equal(4);
+					handler.should.have.been.calledOn(document.body);
+					done();
+				});
+			});
+		});
+
+		describe('drag', function() {
+			it('should work with direct events', function(done) {
+				var handler = sinon.spy();
+				$('body').on(this.pointer.START_EVENT + ' ' + this.pointer.MOVE_EVENT + ' ' + this.pointer.STOP_EVENT, handler);
+				this.pointer.drag(100, 0, function() {
+					handler.callCount.should.be.above(1);
+					handler.should.have.been.calledOn(document.body);
+					done();
+				});
+			});
+
+			it('should work with delegated events', function(done) {
+				var handler = sinon.spy();
+				$('html').on(this.pointer.START_EVENT + ' ' + this.pointer.MOVE_EVENT + ' ' + this.pointer.STOP_EVENT, 'body', handler);
+				this.pointer.drag(100, 0, function() {
+					handler.callCount.should.be.above(1);
+					handler.should.have.been.calledOn(document.body);
+					done();
+				});
+			});
+		});
+
+		describe('flick', function() {
+			it('should work with direct events', function(done) {
+				var handler = sinon.spy();
+				$('body').on(this.pointer.START_EVENT + ' ' + this.pointer.MOVE_EVENT + ' ' + this.pointer.STOP_EVENT, handler);
+				this.pointer.flick(100, 0, function() {
+					handler.callCount.should.be.above(1);
+					handler.should.have.been.calledOn(document.body);
+					done();
+				});
+			});
+
+			it('should work with delegated events', function(done) {
+				var handler = sinon.spy();
+				$('html').on(this.pointer.START_EVENT + ' ' + this.pointer.MOVE_EVENT + ' ' + this.pointer.STOP_EVENT, 'body', handler);
+				this.pointer.flick(100, 0, function() {
+					handler.callCount.should.be.above(1);
+					handler.should.have.been.calledOn(document.body);
+					done();
+				});
+			});
+		});
+	});
+
+}(jQuery));
